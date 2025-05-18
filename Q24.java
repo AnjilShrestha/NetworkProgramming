512b8dd3d758e9a9e67a4e5ec9a2975d87ec4d89
AnjilShrestha <shresthaanjil226@gmail.com>
2025-05-18 08:24:27 +0545

    download webpage using url connection

---
+++ Q24.java
@@ -0,0 +1,26 @@
+
+//download webpage using url connection
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class Q24 {
+    public static void main(String[] args) {
+        try {
+            URL url = new URL("https://www.google.com");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.connect();
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String inputLine;
+            while ((inputLine = in.readLine()) != null) {
+                System.out.println(inputLine);
+            }
+            in.close();
+            connection.disconnect();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
